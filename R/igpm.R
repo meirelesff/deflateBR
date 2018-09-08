dc4df1e840f89347d1063fce3db19bec489672f7
Fernando Meireles <meirelesff@users.noreply.github.com>
2018-09-08 19:51:19 -0300

    New version

--- R/igpm.R
+++ R/igpm.R
@@ -1,7 +1,7 @@
-#' Deflate Nominal Brazilian Reais Using IPCA
+#' Deflate Nominal Brazilian Reais Using IGP-M
 #'
 #' @description
-#' \code{ipca()} is a convenience function to deflate nominal Brazilian Reais using the IBGE's IPCA price index.
+#' \code{ipgm()} is a convenience function to deflate nominal Brazilian Reais using the FGV's IGP-M price index.
 #'
 #' @param nominal_values A \code{numeric} vector containing nominal Brazilian Reais to deflate.
 #' @param nominal_dates A \code{Date} vector with corresponding nominal dates (i.e., when nominal values were measured).
@@ -15,16 +15,16 @@
 #'
 #' @examples
 #' \dontrun{
-#' # Use IPCA index to deflate a vector of nominal Brazilian Reais
+#' # Use IGP-M index to deflate a vector of nominal Brazilian Reais
 #' reais <- rep(100, 5)
 #' actual_dates <- seq.Date(from = as.Date("2001-01-01"), to = as.Date("2001-05-01"), by = "month")
 #'
-#' ipca(reais, actual_dates, "01/01/2018")
+#' igpm(reais, actual_dates, "01/01/2018")
 #' }
 #'
 #' @export
 
-ipca <- function(nominal_values, nominal_dates, real_date){
+igpm <- function(nominal_values, nominal_dates, real_date){
 
-  deflate(nominal_values, nominal_dates, real_date, "ipca")
+  deflate(nominal_values, nominal_dates, real_date, "igpm")
 }
